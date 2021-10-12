985fc710e87c58c86b072950bfa4fc500d3a8086
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2021-10-12 11:53:45 +0100

    Adds count_kmers.cu and count_kmers.cuh for kmer counting from reads.

---
+++ src/count_kmers.cuh
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "containers.cuh"
+#include "hash.cuh"
--- src/countmin.hpp
+++ src/countmin.hpp
@@ -14,12 +14,23 @@ typedef Eigen::Matrix<uint32_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
 // 1. Get function to read from fastq
 // 2. Fill in count_reads_gpu()
 
+// is count_min_pars required or can this be calculated?
+struct count_min_pars {
+    const int width_bits;
+    const int hash_per_hash;
+    uint64_t mask;
+    uint32_t table_width;
+    int table_rows;
+};
+
+// d_count_min already constructed upon constructor of countmin
 class CountMin {
 public:
   CountMin(const std::vector<std::string> &filename, const size_t width,
            const size_t height, const size_t n_threads)
       : width_(width), height_(height), d_count_min_(width * height), d_bloom_(width) {
     seq_ = get_reads(filename, n_threads);
+    // need to flatten std::vector<std::string> seq_ into std::vector<char>
     d_reads_ = device_array<char>(seq_);
     count_reads_gpu();
   }
@@ -51,4 +62,11 @@ private:
   device_array<uint32_t> d_count_min_;
   device_array<uint32_t> d_bloom_;
 
+  // delete move and copy to avoid accidentally using them
+  GPUCountMin(const GPUCountMin &) = delete;
+  GPUCountMin(GPUCountMin &&) = delete;
+
+  // parameters for count_min table
+  count_min_pars _pars;
+  count_min_pars * _d_pars;
 };
